26c97ffa6cb933891ae57c82067ba03c056babff
changoug <49079406+changoug@users.noreply.github.com>
2026-06-07 17:59:20 -0400

    Add: minimum-window-with-characters - submission-5

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        
+        t_count = Counter(t)
+        freq = defaultdict(int)
+
+        L = 0
+        res = [0, 0]
+        curr_min = len(s) + 1
+
+        for R in range(len(s)):
+            freq[s[R]] += 1
+            while all([freq[k] >= t_count[k] for k in t_count]):
+                if curr_min > R - L + 1:
+                    curr_min = R - L + 1
+                    res[0], res[1] = [L, R + 1]
+                freq[s[L]] -= 1
+                L += 1
+
+        return s[res[0]: res[1]]
